09e1cc5f923ccc62bfc8a35b889bfc409af91af5
mkramos <67130752+mkramos@users.noreply.github.com>
2020-07-02 12:21:31 +0800

    Update and rename cmbScanner.js to CMBScanner.js

--- www/cmbScanner.js
+++
@@ -1,7 +0,0 @@
-function cmbScanner() {};
-
-// The function that passes work along to native shells
-cmbScanner.prototype.startScannning = function(successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, 'cmbScanner', 'startScannning', []);
-};
-module.exports = new cmbScanner;
